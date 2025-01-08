09e19a953266c1b1b166dbddc292e57c2d22a5c5
petitlardon <lavie.jul@gmail.com>
2025-01-08 18:53:34 +0100

    manage status in tasks wih dependencies injections

--- src/app/shared/services/tasks.service.ts
+++ src/app/shared/services/tasks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { Task } from '../../tasks/task.model';
+import { Task, TaskStatus } from '../../tasks/task.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +16,9 @@ export class TasksService {
       status: 'OPEN'
     }
     this.tasks.update((oldTasks) => [...oldTasks, newTask])
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTask) => oldTask.map((task) => task.id === taskId ? { ...task, status: newStatus } : task))
   }
 }
